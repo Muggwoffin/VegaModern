4a94b97febdf3b79ee5c9ff86888997e83c46cdc
Muggwoffin <115727701+Muggwoffin@users.noreply.github.com>
2026-03-17 16:05:55 +0000

    Added Doormat functionality
    Used Trigger 'Doormats' to ease transition from one space to another.

---
+++ Assets/Scripts/Player/LocationTrigger.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class LocationTrigger : MonoBehaviour
+{
+    public enum Location {Street, Cafe, Kitchen}
+
+    public Location destination;
+    public AmbienceManager ambienceManager;
+
+    private float lastTriggerTime;
+    private float cooldown = 0.5f;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && Time.time > lastTriggerTime + cooldown)
+            {
+            lastTriggerTime = Time.time;
+
+            switch (destination)
+                {
+                case Location.Street:
+                    ambienceManager.ExitCafe();
+                    break;
+                case Location.Cafe:
+                    ambienceManager.EnterCafe();
+                    break;
+                case Location.Kitchen:
+                    ambienceManager.EnterKitchen();
+                    break;
+                }
+            
+            }
+        Debug.Log("Transitioning to " + destination);
+    }
+}
