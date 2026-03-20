83ed5ddbde87d27883d22be8036b39aede373c05
Muggwoffin <115727701+Muggwoffin@users.noreply.github.com>
2026-03-20 12:12:22 +0000

    Commented the ambience manager

--- Assets/Scripts/Audio/AmbienceManager.cs
+++ Assets/Scripts/Audio/AmbienceManager.cs
@@ -2,12 +2,16 @@ using UnityEngine;
 using UnityEngine.Audio;
 using System.Collections;
 
+//Controls ambient sound zones for street, cafe and kitchen - and also potentially further sound zones
+// Uses AudioMixer parameters to fade smoothly between areas when the player enters/exits them
+// Called by trigger scripts so ambience follows the player's location. These are the doormats.
 public class AmbienceManager : MonoBehaviour
 {
     public AudioMixer mixer;
     public float fadeDuration = 2.0f;
     void Start()
     {
+        // When the game starts only the street vol is audible. But other sounds are not 'off' they are playing in the background silently, this creates a persistent state of audio
         mixer.SetFloat("StreetVol", 0f);
         mixer.SetFloat("CafeVol", -80f);
         mixer.SetFloat("KitchenVol", -80f);
@@ -17,7 +21,7 @@ public class AmbienceManager : MonoBehaviour
 
     public void EnterCafe()
     {
-
+        //Stop ensures there are no in-progress fades that could lead to a conflict as we transition
         StopAllCoroutines();
         StartCoroutine(FadeMixer("StreetVol", -80f, fadeDuration));
         StartCoroutine(FadeMixer("CafeVol", 0f, fadeDuration));
@@ -52,12 +56,15 @@ public class AmbienceManager : MonoBehaviour
         StartCoroutine(FadeMixer("StreetVol", -80f, fadeDuration));
     }
 
+    //This co routine smoothly fades a single exposed parameter in the audio mixer from its current value to target dB over time
     IEnumerator FadeMixer(string param, float targetDb, float duration)
     {
+        //Reads the current value
         float currentDb;
         mixer.GetFloat(param, out currentDb);
         float elapsed = 0f;
 
+        //Graduall moves from the current dB to the targetDb over duration seconds
         while (elapsed < duration)
         {
             elapsed += Time.deltaTime;
