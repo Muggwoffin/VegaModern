a98216ae4fd44321c8f2e6020b916f7b0b798b60
Muggwoffin <115727701+Muggwoffin@users.noreply.github.com>
2026-03-20 11:33:06 +0000

    Commented InpurHandler and Interactor

--- Assets/Scripts/Player/PlayerInputHandler.cs
+++ Assets/Scripts/Player/PlayerInputHandler.cs
@@ -2,18 +2,24 @@ using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+//Keeps in one location all the input from the Unity Input system
+// Boils down moving, looking, sprinting and jumping into simple properties
+// Other scripts (the movement, look, and interactor script) read these each frame instead of talking directly to the Input system.
 public class PlayerInputHandler : MonoBehaviour
 {
     public void Awake()
     {
+        //Cache the PlayerInteractor so we can enable interactions from input
         interactor = GetComponent<PlayerInteractor>();
     }
 
+    //Read only properties that other scripts can query - Vector2s (because move uses WASD and Look uses mouse) and bools because jump and sprint are true false.
     public Vector2 MoveInput {get; private set;}
     public Vector2 LookInput {get; private set;}
     public bool IsBrisk {get; private set;}
     public bool JumpPressed {get; private set;}
     
+    //Handles ray cast based interactions
     private PlayerInteractor interactor;
     
     public void OnMove(InputValue value) 
@@ -45,7 +51,9 @@ public class PlayerInputHandler : MonoBehaviour
     
     public void OnInteract(InputValue value)
     {
+        // Only trigger on the press, not on release
         if (!value.isPressed) return;
+        //Ask the player interactor to try interacting with whatever is in front of the player. Basically a shorthand version of an if statement
         interactor?.TryInteract();
     }
 }
--- Assets/Scripts/Player/PlayerInteractor.cs
+++ Assets/Scripts/Player/PlayerInteractor.cs
@@ -2,12 +2,18 @@ using System;
 using UnityEngine;
 using UnityEngine.UI;
 
+//Handles looking at and interacting with objects that are in front of the player.
+// Shoots out a ray cast from the Camera Transform that checks if an object is linked to the IInteractable interface within a short range of the player
+// Uses the UI system to change the reticle colour to provide a visual cue that the player can interact with this object
 public class PlayerInteractor : MonoBehaviour
 {
 
     [SerializeField] private Transform cameraTransform;
     [SerializeField] private float interactRange = 3f;
+    //Only hit objects that are within the interactable layer
     [SerializeField] private LayerMask interactableLayer;
+    
+    [Header("Reticle UI")]
     [SerializeField] private Image reticleImage;
     [SerializeField] private Color normalReticleColor = Color.beige;
     [SerializeField] private Color interactReticleColor = Color.green;
@@ -19,10 +25,13 @@ public class PlayerInteractor : MonoBehaviour
 
     public void TryInteract()
     {
+        // Builds a ray starting at the camera that looks forward
         Ray ray = new Ray(cameraTransform.position, cameraTransform.forward);
 
+        // Hit the ray forward and see if it hits something, hemmed in by our range and layers
         if (Physics.Raycast(ray, out RaycastHit hit, interactRange, interactableLayer))
         {
+            //Check if the object is part of the interactable interface
             if (hit.collider.TryGetComponent(out IInteractable interactable))
             {
                 interactable.Interact();
@@ -37,12 +46,14 @@ public class PlayerInteractor : MonoBehaviour
         
         if (Physics.Raycast(ray, out RaycastHit hit, interactRange, interactableLayer))
             {
+                //If the ray hits an object on the interactable range, the colour of the reticle changes green
                 if (hit.collider.TryGetComponent(out IInteractable _))
                 {
                     reticleImage.color = interactReticleColor;
                     return;
                 }
             }
+        //otherwise just keep the reticle white.
         reticleImage.color = normalReticleColor;
     }
 }
